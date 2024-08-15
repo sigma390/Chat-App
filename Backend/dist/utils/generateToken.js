48335d8644b25b326349cff26436248cc9a22b9d
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-15 22:46:11 +0530

    change

--- Backend/dist/utils/generateToken.js
+++ Backend/dist/utils/generateToken.js
@@ -3,11 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.secret = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const secret = process.env.JWT_SECRET;
+exports.secret = process.env.JWT_SECRET;
 const secuirity = process.env.NODE_ENV === 'production' ? true : false;
 const generatejwt = (userId, res) => {
-    const token = jsonwebtoken_1.default.sign({ userId }, secret, { expiresIn: '1h' });
+    const token = jsonwebtoken_1.default.sign({ userId }, exports.secret, { expiresIn: '1h' });
     res.cookie("JWT", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000, //milisecs
         httpOnly: true, //prevent xss attacks
