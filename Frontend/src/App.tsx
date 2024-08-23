c0fec371d8a12f7426fa637adaa14c0f9e07ba85
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-23 20:32:27 +0530

    Message and Message Container Done!!!

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css';
+import Home from './pages/home/Home';
 // import Home from './pages/home/Home'
 // import Signup from './pages/signup/Signup'
-import Login from './pages/login/Login';
+// import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center '>
-      <Login />
-      {/* <Signup/> */}
-      {/* <Home/> */}
+      {/* <Login />
+      <Signup/> */}
+      <Home />
     </div>
   );
 }
