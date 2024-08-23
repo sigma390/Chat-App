9ee3b5c079f93b59b4ea60f49b574e5a4b6d3edd
Omkar Patil <git21cs072-t@students.git.edu>
2024-08-23 20:16:10 +0530

    Added Logout Button and Styling to Sidebar

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,27 +1,16 @@
-
-import './App.css'
+import './App.css';
 // import Home from './pages/home/Home'
 // import Signup from './pages/signup/Signup'
-import Login from './pages/login/Login'
-
+import Login from './pages/login/Login';
 
 function App() {
-
-
   return (
-    
-      <div className="p-4 h-screen flex items-center justify-center ">
-        <Login/>
-        {/* <Signup/> */}
-        {/* <Home/> */}
-
-
-       
-       
-      </div>
-       
-    
-  )
+    <div className='p-4 h-screen flex items-center justify-center '>
+      <Login />
+      {/* <Signup/> */}
+      {/* <Home/> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/components/sidebar/Sidebar.tsx
+++ Frontend/src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,16 @@
-
-import Conversations from './Conversations'
-import SearchInpt from './SearchInpt'
+import Conversations from './Conversations';
+import Logout from './Logout';
+import SearchInpt from './SearchInpt';
 
 const Sidebar = () => {
   return (
-    <div>
-        <SearchInpt/>
-        <Conversations/>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
+      <SearchInpt />
+      <div className='divider px-3'></div>
+      <Conversations />
+      <Logout />
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
