dfa554be5472dc189a111cc0533c9133ecb8b579
Omkar Patil <git21cs072-t@students.git.edu>
2024-09-18 23:00:26 +0530

    Updated Fetch Request

--- Frontend/src/components/sidebar/Conversations.tsx
+++ Frontend/src/components/sidebar/Conversations.tsx
@@ -1,15 +1,18 @@
-import Convo from "./Convo";
+import useGetConversations from '../../hooks/useGetConversations';
+import Convo from './Convo';
 
 const Conversations = () => {
-	return (
-		<div className='py-2 flex flex-col overflow-auto'>
-			<Convo />
-			<Convo />
-			<Convo />
-			<Convo />
-			<Convo />
-			<Convo />
-		</div>
-	);
+  const { loading, conversations } = useGetConversations();
+  console.log(conversations);
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+      <Convo />
+      <Convo />
+      <Convo />
+      <Convo />
+      <Convo />
+      <Convo />
+    </div>
+  );
 };
 export default Conversations;
